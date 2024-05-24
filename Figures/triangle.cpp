956b05dbdbd0a479d1fe8c92b1a1fedef1759f06
Zelmoron <ak.igor13@mail.ru>
2024-05-24 12:44:34 +0800

    ~

--- Figures/triangle.cpp
+++ Figures/triangle.cpp
@@ -14,6 +14,12 @@ double Triangle::calc_area()
 	Geron = sqrt(pol_p * (pol_p - width1) * (pol_p - width2) * (pol_p - width3));
 	return Geron;
 }
+Triangle::Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+	width1 = sqrt((pow((x2 - x1), 2) + pow((y2 - y1), 2)));
+	width2 = sqrt((pow((x3 - x1), 2) + pow((y3 - y1), 2)));
+	width3 = sqrt((pow((x3 - x2), 2) + pow((y3 - y2), 2)));
+}
 
 void Triangle::name()
 {
--- Figures/triangle.h
+++ Figures/triangle.h
@@ -4,14 +4,16 @@
 #include <iostream>
 class Triangle :public Geometric_Figure {
 public:
-	Triangle_sides(double w1,double w2,double w3):width1(w1), 
-		width2(w2), 
+	Triangle(double w1, double w2, double w3) :width1(w1),
+		width2(w2),
 		width3(w3) {}
 	double calc_perimetr() override;
 	double calc_area() override;
 	void name() override;
+	Triangle(double x1, double y1, double x2, double y2,
+		double x3, double y3);
 private:
-	double width1 ;
-	double width2 ;
-	double width3 ;
+	double width1;
+	double width2;
+	double width3;
 };
